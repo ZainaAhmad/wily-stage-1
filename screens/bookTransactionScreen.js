d39820a74db920ae7f033df8fd1810b1b9eb8cfd
ZainaAhmad <63003618+ZainaAhmad@users.noreply.github.com>
2020-12-09 19:49:42 +0530

    Add files via upload

---
+++ screens/bookTransactionScreen.js
@@ -0,0 +1,13 @@
+import {View,Text,Stylesheet} from 'react-native'
+import React from 'react'
+export default class TransactionScreen extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:'center',alignItems:"center"}}>
+                <Text>
+                    Issue or return
+                </Text>
+            </View>
+        )
+    }
+}
